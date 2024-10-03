dbb02ad751d43a9d9405a2b9246af9e9a11ab031
Guilherme Colhado Souza <guilherme.goncalves@jala.university>
2024-10-03 11:43:52 -0300

    feat

--- src/presentation/controllers/QuoteController.js
+++ src/presentation/controllers/QuoteController.js
@@ -2,7 +2,6 @@ export class QuoteController {
   constructor(getRandomQuote) {
     this.getRandomQuoteUseCase = getRandomQuote;
   }
-
   getRandomQuote() {
     const quote = this.getRandomQuoteUseCase.execute();
     return `"${quote.text}" - ${quote.character} (${quote.anime})`;
